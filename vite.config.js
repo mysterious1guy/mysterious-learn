0c99a16371cd65706dd94938a4526bd49ebf10c8
mysterious1guy <mouhamedfall@esp.sn>
2026-02-18 15:07:23 +0000

    Add allowedHosts configuration for server

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   preview: {
+    allowedHosts: 'all'
+  },
+  server: { // <-- Ajoutez cette section
     allowedHosts: 'all'
   }
 })
